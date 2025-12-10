9484cbc4d85397efc702d545f450c6a8e50ffeb5
julia <julia@HORRORBREAKDOWN>
2025-12-10 11:54:06 -0500

    new

---
+++ AquaTrack/Database/StoredProcedures/Top5OldestFish.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR ALTER PROCEDURE dbo.Top5OldestFish
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT TOP 5
+        Id,
+        Name,
+        Species,
+        Age,
+        TankId
+    FROM Fish
+    ORDER BY Age DESC;
+END
---
+++ AquaTrack/Models/OldestFishResult.cs
@@ -0,0 +1,6 @@
+﻿namespace AquaTrack.Models
+{
+    public class OldestFishResult
+    {
+    }
+}
